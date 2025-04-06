63def5b3ea45c721d0a6b093b68197719373cc70
Naween <naweendilshan26@gmail.com>
2025-04-06 18:29:03 +0530

    cross origin bug fix
    cross origin bug fix

--- src/main/java/com/example/koratuwabackend/config/CorsConfig.java
+++ src/main/java/com/example/koratuwabackend/config/CorsConfig.java
@@ -6,17 +6,39 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.Arrays;
+
+//@Configuration
+//public class CorsConfig {
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowCredentials(true);
+//        config.addAllowedOrigin("http://localhost:3000"); // Allow requests from this origin
+//        config.addAllowedHeader("*");
+//        config.addAllowedMethod("*");
+//        source.registerCorsConfiguration("/**", config);
+//        return new CorsFilter(source);
+//    }
+//}
+
 @Configuration
 public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:3000"); // Allow requests from this origin
+        config.setAllowedOrigins(Arrays.asList(
+                "http://localhost:3000",
+                "https://sweet-taiyaki-2ea072.netlify.app"
+        ));
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
+
         return new CorsFilter(source);
     }
 }
